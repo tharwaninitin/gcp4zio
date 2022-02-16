732f651b46f30cb7e236442d4adb53c9563675ae
Nitin Tharwani <tharwaninitin182@gmail.com>
2022-02-17 01:06:11 +0530

    Update Versions.scala

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,15 +1,15 @@
 object Versions {
   val scala212                  = "2.12.15"
   val scala213                  = "2.13.7"
-  val scala3                    = "3.1.0"
+  val scala3                    = "3.1.1"
   val scala2Versions            = List(scala212, scala213)
   val allScalaVersions          = List(scala212, scala213, scala3)
-  val Gcp4ZioVersion            = "0.1.1"
+  val Gcp4ZioVersion            = "0.2.0"
   val ZioVersion                = "1.0.13"
-  val Sl4jVersion               = "1.7.35"
+  val Sl4jVersion               = "1.7.36"
   val LogbackVersion            = "1.2.10"
   val ScalaJavaCollectionCompat = "2.6.0"
-  val GcpBqVersion              = "2.7.1"
-  val GcpDpVersion              = "2.3.1"
-  val GcpGcsVersion             = "2.3.0"
+  val GcpBqVersion              = "2.9.0"
+  val GcpDpVersion              = "2.3.2"
+  val GcpGcsVersion             = "2.4.2"
 }
