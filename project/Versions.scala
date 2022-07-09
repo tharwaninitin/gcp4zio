b04758a4f1f297ec3b4df92ce7c78a8f40a6e709
Nitin Tharwani <tharwaninitin182@gmail.com>
2022-07-10 02:04:29 +0530

    Updated all libraries to latest version

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,14 +1,14 @@
 object Versions {
   val scala211                  = "2.11.12"
   val scala212                  = "2.12.15"
-  val scala213                  = "2.13.7"
+  val scala213                  = "2.13.8"
   val scala3                    = "3.1.1"
   val allScalaVersions          = List(scala211, scala212, scala213, scala3)
   val ZioVersion                = "2.0.0"
   val Sl4jVersion               = "1.7.36"
   val LogbackVersion            = "1.2.11"
-  val ScalaJavaCollectionCompat = "2.7.0"
-  val GcpBqVersion              = "2.13.6"
-  val GcpDpVersion              = "3.1.2"
-  val GcpGcsVersion             = "2.8.1"
+  val ScalaJavaCollectionCompat = "2.8.0"
+  val GcpBqVersion              = "2.13.8"
+  val GcpDpVersion              = "4.0.0"
+  val GcpGcsVersion             = "2.9.3"
 }
