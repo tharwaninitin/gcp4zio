f613f2e6cd753bed26b0e3763678406bbf788c43
Nitish N Banakar <53825720+nitishnb@users.noreply.github.com>
2023-05-08 13:14:15 +0530

    Updating dependecy version

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,16 +4,16 @@ object Versions {
   val Scala3           = "3.2.1"
   val AllScalaVersions = List(Scala212, Scala213, Scala3)
 
-  val ScalaJavaCollectionCompat = "2.9.0"
+  val ScalaJavaCollectionCompat = "2.10.0"
 
-  val ZioVersion    = "2.0.9"
-  val ZioLogVersion = "2.1.10"
+  val ZioVersion    = "2.0.13"
+  val ZioLogVersion = "2.1.12"
 
-  val LogbackVersion      = "1.4.5"
-  val GcpBqVersion        = "2.23.0"
-  val GcpDpVersion        = "4.8.0"
-  val GcpGcsVersion       = "2.19.0"
-  val GcpPubSubVersion    = "1.123.4"
-  val CloudMonitorVersion = "3.12.0"
-  val BatchVersion        = "0.11.0"
+  val LogbackVersion      = "1.4.7"
+  val GcpBqVersion        = "2.25.0"
+  val GcpDpVersion        = "4.13.0"
+  val GcpGcsVersion       = "2.22.1"
+  val GcpPubSubVersion    = "1.123.11"
+  val CloudMonitorVersion = "3.17.0"
+  val BatchVersion        = "0.16.0"
 }
