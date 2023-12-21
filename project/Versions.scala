a274bfa2ed2867e9d6d9366d9cabb493b02ad7c2
Nitin Tharwani <tharwaninitin182@gmail.com>
2023-12-21 10:34:28 +0530

    Updated dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,14 +1,14 @@
 object Versions {
-  val scala212 = "2.12.17"
-  val scala213 = "2.13.10"
-  val scala3   = "3.2.1"
+  val scala212 = "2.12.18"
+  val scala213 = "2.13.12"
+  val scala3   = "3.3.1"
 
   val allScalaVersions: List[String] = List(scala212, scala213, scala3)
 
-  val scalaJavaCollectionCompat = "2.10.0"
+  val scalaJavaCollectionCompat = "2.11.0"
 
-  val zioVersion    = "2.0.13"
-  val zioLogVersion = "2.1.12"
+  val zioVersion    = "2.0.20"
+  val zioLogVersion = "2.1.16"
 
   val logbackVersion      = "1.4.14"
   val gcpBqVersion        = "2.35.0"
